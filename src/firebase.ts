4b903712fcfe98f9e88e5eb2c3fff07fa2d31575
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-12-08 22:57:33 +0900

    ✨ feat: firebase로 인증 과정 추가

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyATNsLLaOPQ4jrUIyDhRW9K3wMRuFMZItA",
   authDomain: "nwitter-reloaded-450d3.firebaseapp.com",
@@ -13,5 +10,6 @@ const firebaseConfig = {
   appId: "1:774734146271:web:db2cfbf01a10bbc7d7976a",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
