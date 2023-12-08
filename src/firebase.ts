4a3dd690a787ce9d0be5208adb7e891c4ab2526a
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-12-08 22:53:16 +0900

    ✨ feat: firebase 초기 설정 로직 추가

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATNsLLaOPQ4jrUIyDhRW9K3wMRuFMZItA",
+  authDomain: "nwitter-reloaded-450d3.firebaseapp.com",
+  projectId: "nwitter-reloaded-450d3",
+  storageBucket: "nwitter-reloaded-450d3.appspot.com",
+  messagingSenderId: "774734146271",
+  appId: "1:774734146271:web:db2cfbf01a10bbc7d7976a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
